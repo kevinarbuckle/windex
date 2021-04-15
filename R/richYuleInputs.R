f5836e7c54c0214e2beff86fe859f0ae6324431a
kevinarbuckle <72972690+kevinarbuckle@users.noreply.github.com>
2021-04-15 11:06:01 +0100

    Add files via upload

---
+++ R/richYuleInputs.R
@@ -0,0 +1,21 @@
+richYuleInputs<-function(tree,x){
+if(class(tree)!="phylo") stop('Tree must be of class phylo')
+intNodes=(Ntip(tree)+1L):(Ntip(tree)+Nnode(tree))
+sisters=Children(tree,intNodes)
+sisters=matrix(unlist(sisters),byrow=TRUE,ncol=2,dimnames=list(intNodes,NULL))
+desc=Descendants(tree)
+x=(x=="1")
+l=sapply(desc,length)
+yes=sapply(desc,function(d,x)sum(x[d]),x)
+no=(l-yes)
+allno=(no==l)
+allyes=(yes==l)
+res=rbind(cbind(no[sisters[,1]],yes[sisters[,2]])[allno[sisters[,1]]&allyes[sisters[,2]],],cbind(no[sisters[,2]],yes[sisters[,1]])[allno[sisters[,2]]&allyes[sisters[,1]],])
+res=as.data.frame(res)
+res<-data.frame("with"=res[,2],"without"=res[,1])
+sisnodes=c(rownames(sisters)[allno[sisters[,1]]&allyes[sisters[,2]]],rownames(sisters)[allno[sisters[,2]]&allyes[sisters[,1]]])
+nodeages<-max(node.depth.edgelength(tree))-node.depth.edgelength(tree)
+tree$node.label<-nodeages[(length(nodeages)-(Nnode(tree)-1)):length(nodeages)]
+sisdivtim=as.numeric(tree$node.label[as.numeric(sisnodes)-Ntip(tree)])
+return(list(sisRich=res,divTimes=sisdivtim))
+}
