b6f6473508fabc5882b5fe3ba346b512992d3a26
kevinarbuckle <72972690+kevinarbuckle@users.noreply.github.com>
2023-10-11 11:30:08 +0100

    Update nodeDist.R

--- R/nodeDist.R
+++ R/nodeDist.R
@@ -1,4 +1,4 @@
-nodeDist<-function(trees,sp1,sp2,relTime=F,fillcol="blue",main="",...){
+nodeDist<-function(trees,sp1,sp2,relTime=F,fillcol="blue",xlabel="Age (mya)",main="",add=F,...){
 ages<-c()
 for (i in 1:length(trees)){
 time.from.root<-findMRCA(trees[[i]],tips=c(sp1,sp2),type="height")
@@ -7,6 +7,10 @@ if (relTime==F) ages[i]<-age.of.root-time.from.root
 else ages[i]<-(age.of.root-time.from.root)/age.of.root
 }
 agedist<-density(ages)
-plot(agedist,xlab="Age (mya)",main=main,...)
+if (add==F) plot(agedist,xlab=xlabel,main=main,...)
+else {
+lines(agedist)
+print("As you are adding a distribution to an existing plot, make sure xlim and ylim on original plot are set to accomodate all plotted distributions")
+}
 polygon(agedist,col=fillcol)
 }
