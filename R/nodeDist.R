f98de9bf749e014722cae97795d43dc9e7f930b0
kevinarbuckle <72972690+kevinarbuckle@users.noreply.github.com>
2024-04-26 17:16:53 +0100

    Update nodeDist.R

--- R/nodeDist.R
+++ R/nodeDist.R
@@ -1,6 +1,6 @@
 nodeDist<-function(trees,sp1,sp2,relTime=F,fillcol="blue",xlabel="Age (mya)",main="",return.ages=F,plot=T,add=F,...){
 tree<-trees
-if (is.phylo(trees)==T) trees<-c(trees,trees)
+if (is(trees,"phylo")) trees<-c(trees,trees)
 ages<-c()
 for (i in 1:length(trees)){
 time.from.root<-findMRCA(trees[[i]],tips=c(sp1,sp2),type="height")
@@ -16,10 +16,10 @@ lines(agedist)
 print("As you are adding a distribution to an existing plot, make sure xlim and ylim on original plot are set to accomodate all plotted distributions")
 }
 polygon(agedist,col=fillcol)
-if (is.phylo(tree)==T) warning("This distribution is based on a single age estimate as only one tree (not a set) was provided")
+if (is(trees,"phylo")) warning("This distribution is based on a single age estimate as only one tree (not a set) was provided")
 }
 if (return.ages==T) {
-if (is.phylo(tree)==T) ages[1]
+if (is(tree,"phylo")) ages[1]
 else ages
 }
 }
